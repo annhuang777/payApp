2de681a30387df7265a20f876c0423648c2fe000
annhuang <ann.huang777@gmail.com>
2020-05-27 20:34:19 +0800

    Initial Commit

---
+++ payApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  payApp
+//
+//  Created by Pin yu Huang on 2020/5/27.
+//  Copyright © 2020 Pin yu Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
